de10bbb1cf1b02695bb487639cf2883b34865a2a
DianaCelis <dscelis@misena.edu.co>
2022-12-06 18:45:00 -0500

    terminando ejercicios

--- src/clinica/reto2Prueba.java
+++ src/clinica/reto2Prueba.java
@@ -4,8 +4,8 @@ import salud.reto2;
 public class reto2Prueba {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
-        reto2Prueba pesopaciente=new reto2Prueba();
-        
+        reto2 pesopaciente=new reto2();
+        pesopaciente.calcularlmc();
     }
 
 }
